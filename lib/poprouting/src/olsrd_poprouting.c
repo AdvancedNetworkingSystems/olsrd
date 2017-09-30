4bcd1666850cb1f898f81b7392e6909dd0f2d25d
Nicolo' Facchi <nicolo.facchi@gmail.com>
2017-09-30 15:50:34 +0200

    Set TC validity time to 30 times the TC interval

--- lib/poprouting/src/olsrd_poprouting.c
+++ lib/poprouting/src/olsrd_poprouting.c
@@ -149,7 +149,7 @@ void set_tc_timer(struct autobuf *abuf) {
     in->interf->tc_gen_timer->timer_period = timer * MSEC_PER_SEC;
     in->interf->tc_gen_timer->timer_jitter_pct = POPROUTING_JITTER; // Jitter to 5%
     in->cnf->tc_params.emission_interval = timer;
-    in->cnf->tc_params.validity_time = timer*60;
+    in->cnf->tc_params.validity_time = timer*30;
     in->interf->valtimes.tc=reltime_to_me(in->cnf->tc_params.validity_time * MSEC_PER_SEC);
     in = in->next;
   }
